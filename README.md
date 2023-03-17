0bcbc2bcf442952813a987f8704171c96af67acb
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-03-17 20:11:18 +0530

    Add README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# IEEE Project on Global Connectivity Framework and 5G IoT Resource Sharing using Game Theory
+
+## Objective
+
+To develop an effective solution that reduces network traffic at different nodes of a particular region and evenly redistribute the load, achieving uniform 5G network connectivity.
+
+## Duration of Project
+
+May 2022 - July 2022
--- csv_for_nodes.py
+++ csv_for_nodes.py
@@ -2,24 +2,23 @@
 
 nodes = {}
 
-file = open("F:\Python Programming\IEEE Task\Tata_Region_Nodes.txt","r")
+file = open("F:\Python Programming\IEEE Task\Tata_Region_Nodes.txt", "r")
 lines = file.readlines()
 
-for i in range(0,len(lines),8):
-    
+for i in range(0, len(lines), 8):
+
     if "node" in lines[i]:
-        name = lines[i+2].split()[1].replace("\"","")
+        name = lines[i+2].split()[1].replace("\"", "")
         nodes[name] = {}
-        
-        country = lines[i+3].split()[1].replace("\"","")
+
+        country = lines[i+3].split()[1].replace("\"", "")
         nodes[name]["Country"] = country
-        
+
         long = float(lines[i+4].split()[1])
         nodes[name]["Longitude"] = long
-        
+
         lat = float(lines[i+6].split()[1])
         nodes[name]["Latitude"] = lat
-        
-        
-print(nodes)
 
+
+print(nodes)
